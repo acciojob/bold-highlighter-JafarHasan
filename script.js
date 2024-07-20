34ded2e41c1ff80a2e472493ed47ebc77752589e
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-20 22:38:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
     //Write your code here
+	const all=document.querySelectorAll('strong');
+	all.forEach((element)=>{
+		element.style.color="Green";
+	})
 
 
 }
 
 
 function return_normal() {
+	const all=document.querySelectorAll('strong');
     //Write your code here
+	all.forEach((element)=>{
+		element.style.color="black";
+	})
 
     
 }
